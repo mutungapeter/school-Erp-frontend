1c6e393c18759a6f7aa3786bef82666fd20d1644
mutungapeter <petersiku99@gmail.com>
2024-11-23 13:09:56 +0300

    minor changes

--- src/components/ReportsForms/reportPdf.tsx
+++ src/components/ReportsForms/reportPdf.tsx
@@ -52,6 +52,7 @@ const styles = StyleSheet.create({
     textDecoration: "underline",
     justifyContent: "center",
     marginTop: 20,
+    textTransform: "uppercase",
   },
   studentContainer: {
     marginBottom: 30,
