3fb481c2f4b1b8d7863d08536f9f160bc848e20a
peter mutunga <petersiku99@gmail.com>
2025-01-18 06:16:28 +0300

    minor changes

--- src/components/merit-list/meritListPdf.tsx
+++ src/components/merit-list/meritListPdf.tsx
@@ -1,11 +1,12 @@
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 import { Report } from "@/src/definitions/marks";
-
+import { Image } from "@react-pdf/renderer";
 const styles = StyleSheet.create({
-  page: {   
+  page: {
     flexDirection: "column",
     padding: 10,
-    fontFamily: "Helvetica",},
+    fontFamily: "Helvetica",
+  },
   table: {
     marginTop: 10,
     borderStyle: "solid",
@@ -22,7 +23,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 3,
     textAlign: "left",
-    fontSize: 10, 
+    fontSize: 10,
     borderRightWidth: 1,
     borderRightColor: "black",
     borderRightStyle: "solid",
@@ -31,7 +32,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 3,
     textAlign: "center",
-    fontSize: 10, 
+    fontSize: 10,
   },
   heading: {
     fontSize: 14,
@@ -53,7 +54,7 @@ const styles = StyleSheet.create({
     textTransform: "uppercase",
   },
   header1: {
-    fontSize: 14,
+    fontSize: 10,
     fontWeight: "bold",
     textAlign: "center",
     textTransform: "uppercase",
@@ -65,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   subHeader: {
-    fontSize: 12,
+    fontSize: 10,
     fontWeight: "bold",
     textAlign: "center",
     textDecoration: "underline",
@@ -73,63 +74,69 @@ const styles = StyleSheet.create({
     marginTop: 20,
     textTransform: "uppercase",
   },
+  schoolLogo: {
+    width: 90,
+    height: 90,
+  },
 });
 
 export const MeritListPDF = ({ reports }: { reports: Report[] }) => {
+  const firstReport = reports.length > 0 ? reports[0] : undefined;
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.headerContainer}>
+          <Image style={styles.schoolLogo} src="/images/logo.jpg" />
+          <View style={{ alignItems: "center" }}>
+            <Text style={styles.header}>KWAMWATU SECONDARY SCHOOL </Text>
+            <View style={styles.subHeaderSection}>
+              <Text style={styles.header1}>
+                MERIT LIST - {firstReport?.student.class_level.name}{" "}
+                {firstReport?.student.class_level?.stream?.name}
+              </Text>
 
-    const firstReport = reports.length > 0 ? reports[0] : undefined;
-    return (
-    
-      <Document>
-        <Page size="A4" style={styles.page}>
-    
-          <>
-          <View style={styles.headerContainer}>
-    
-                  <View style={{ alignItems: "center" }}>
-                      <Text style={styles.subHeaderSection}>KWAMWATU SECONDARY SCHOOL </Text>
-                      <View style={styles.header}>
-                            <Text> MERIT LIST - 
-                                {firstReport?.student.class_level.name}{" "}
-                                {firstReport?.student.class_level?.stream?.name}
-                            </Text>
-                            <Text style={styles.subHeader}>
-                                {firstReport?.marks[0]?.term.term || "-"} - 
-                                {firstReport?.student.class_level.calendar_year || "-"}
-                            </Text>
-                        </View>
-                  </View>
-              </View><View style={styles.table}>
-                      {/* Table Header */}
-                      <View style={styles.tableRow}>
-                          {/* <Text style={styles.tableCell}>#</Text> */}
-                          <Text style={styles.tableCell}>Name</Text>
-                          <Text style={styles.tableCell}>Admission Number</Text>
-                          <Text style={styles.tableCell}>Mean Grade</Text>
-                          <Text style={styles.tableCellLast}>Position</Text>
-                      </View>
-    
-    
-                      {reports.map((report, index) => (
-                      <View key={index} style={styles.tableRow}>
-                          {/* <Text style={styles.tableCell}>{index + 1}</Text> */}
-                          <Text style={styles.tableCell}>
-                              {report.student.first_name} {report.student.last_name}
-                          </Text>
-                          <Text style={styles.tableCell}>{report.student.admission_number}</Text>
-                          <Text style={styles.tableCell}>
-                              {report.overall_grading.mean_grade}
-                          </Text>
-                          <Text style={styles.tableCellLast}>
-                              {report.overall_grading.position}
-                          </Text>
-                      </View>
-     ))}
-                  </View>
-                  </>
-    
-        </Page>
-      </Document>
-    );
-}
+              <Text style={styles.subHeader}>
+                {firstReport?.marks[0]?.term.term || "-"} -
+                {firstReport?.student.class_level.calendar_year || "-"}
+              </Text>
+            </View>
+          </View>
+        </View>
 
+     
+        <View style={styles.table}>
+     
+          <View style={styles.tableRow}>
+            {/* <Text style={styles.tableCell}>#</Text> */}
+            <Text style={styles.tableCell}>Name</Text>
+            <Text style={styles.tableCell}>Admission No</Text>
+            <Text style={styles.tableCell}>Mean points</Text>
+            <Text style={styles.tableCell}>Mean grade</Text>
+            <Text style={styles.tableCellLast}>Position</Text>
+          </View>
+
+          {reports.map((report, index) => (
+            <View key={index} style={styles.tableRow}>
+              {/* <Text style={styles.tableCell}>{index + 1}</Text> */}
+              <Text style={styles.tableCell}>
+                {report.student.first_name} {report.student.last_name}
+              </Text>
+              <Text style={styles.tableCell}>
+                {report.student.admission_number}
+              </Text>
+              <Text style={styles.tableCell}>
+              {report.overall_grading.mean_points}
+              </Text>
+              <Text style={styles.tableCell}>
+                {report.overall_grading.mean_grade}
+              </Text>
+              <Text style={styles.tableCellLast}>
+                {report.overall_grading.position}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </Page>
+    </Document>
+  );
+};
