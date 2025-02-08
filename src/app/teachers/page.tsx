ab5c27d659b049458c30a287a85632048d404979
peter mutunga <petersiku99@gmail.com>
2025-02-08 06:51:05 +0300

    minor changes

--- src/app/teachers/page.tsx
+++ src/app/teachers/page.tsx
@@ -3,18 +3,23 @@ import DefaultLayout from "@/src/components/adminDashboard/Layouts/DefaultLayout
 import PageLoadingSpinner from "@/src/components/layouts/PageLoadingSpinner"
 import Teachers from "@/src/components/teachers/Teachers"
 import dynamic from "next/dynamic"
+import { useAppSelector } from "@/redux/hooks";
+import { RootState } from "@/redux/store";
 import { Suspense } from "react"
+import TeacherLayout from "@/src/components/teacherDashboard/TeacherLayout";
 
 const TeachersPage=()=>{
- 
+ const{ user, loading } = useAppSelector((state: RootState) => state.auth);
+ const Layout = user?.role === "Teacher" ? TeacherLayout : DefaultLayout;
+  
   return (
    
 
-    <DefaultLayout>
+    <Layout>
        <Suspense fallback={<PageLoadingSpinner />}>
       <Teachers />
        </Suspense>
-    </DefaultLayout>
+    </Layout>
   
   )
 }
